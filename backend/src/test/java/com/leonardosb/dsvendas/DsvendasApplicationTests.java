a5bf7c6c8b9d17b7c49f653f55d1c76f7c2e2fc9
LeonardoScherer <leoblacena11@gmail.com>
2021-05-03 09:19:39 -0300

    Project created

---
+++ backend/src/test/java/com/leonardosb/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardosb.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
